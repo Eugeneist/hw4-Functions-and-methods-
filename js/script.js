83226ea581c6f707f77d0917592ccfd48ba01a19
Yevhen Rudofylov <rudofiloveugene@gmail.com>
2022-02-06 17:51:01 +0200

    added first function of task 4

--- js/script.js
+++ js/script.js
@@ -2,65 +2,72 @@
 
 // ЗАДАЧА 1
 
-// let searchString = (originalStr, str) => originalStr.includes(str);
+let searchString = (originalStr, str) => originalStr.includes(str);
 
-// console.log(searchString("подушка", "душ"));
-// console.log(searchString("Hello", "llo"));
-// console.log(searchString("Hello", "www"));
+console.log(searchString("подушка", "душ"));
+console.log(searchString("Hello", "llo"));
+console.log(searchString("Hello", "www"));
 
 
 // ЗАДАЧА 2
 
-// let capitalizeFirstLetter = (str) => {
-//    return str[0].toUpperCase() + str.substr(1, str.length);
-// }
+let capitalizeFirstLetter = (str) => {
+   return str[0].toUpperCase() + str.substr(1, str.length);
+}
 
-// let userText = prompt("Введите слово с маленькой буквы");
+let userText = prompt("Введите слово с маленькой буквы");
 
-// alert(capitalizeFirstLetter(userText));
+alert(capitalizeFirstLetter(userText));
 
 
 // ЗАДАЧА 3
 
-// let str = prompt("Введите строку");
-// let maxlength = prompt("Введите максимальную длинну строки!");
+let str = prompt("Введите строку");
+let maxlength = prompt("Введите максимальную длинну строки!");
 
-// function truncate(str, maxlength) {
-//     if (str.length <= maxlength){
-//         return str;
-//     } else {
-//         return str.substr(0, maxlength) + "...";
-//     }
-// }
+function truncate(str, maxlength) {
+    if (str.length <= maxlength){
+        return str;
+    } else {
+        return str.substr(0, maxlength) + "...";
+    }
+}
 
-// console.log(truncate(str, maxlength));
+console.log(truncate(str, maxlength));
 
 
 // ЗАДАЧА 4
 
-// const students = [
-//     {
-//         name: 'John Smith',
-//         marks: [10, 8, 6, 9, 8, 7 ]
-//     },
-//     {
-//         name: 'John Doe',
-//         marks: [ 9, 8, 7, 6, 7 ]
-//     },
-//     {
-//         name: 'Thomas Anderson',
-//         marks: [6, 7, 10, 8 ]
-//     },
-//     {
-//         name: 'Jean-Baptiste Emanuel Zorg',
-//         marks: [10, 9, 8, 9 ]
-//     }
-// ]
+const students = [
+    {
+        name: 'John Smith',
+        marks: [10, 8, 6, 9, 8, 7 ]
+    },
+    {
+        name: 'John Doe',
+        marks: [ 9, 8, 7, 6, 7 ]
+    },
+    {
+        name: 'Thomas Anderson',
+        marks: [6, 7, 10, 8 ]
+    },
+    {
+        name: 'Jean-Baptiste Emanuel Zorg',
+        marks: [10, 9, 8, 9 ]
+    }
+]
 
-// let addAvarageMark = 
+const getAvarageMark = (array) => {
+    const studentsList = array.map(a => Object.assign({}, a));
 
+    studentsList.forEach(object => {
+        let average = object.marks.reduce((a, b) => (a + b)) / object.marks.length;
+        object.averageMark = `${average}`;
+    });
+    console.log(studentsList);
+}
 
-
+getAvarageMark(students);
 
 
 
